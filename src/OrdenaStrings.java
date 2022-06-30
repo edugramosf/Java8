ee8acc773cac6f3b9e165ee520834c9c4a7f9327
Eduardo Ramos <edugramosf@gmail.com>
2022-06-30 17:30:48 -0300

    Que venham os lambdas!

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -7,32 +7,58 @@ import java.util.function.Consumer;
 public class OrdenaStrings {
     public static void main(String[] args) {
 
-       List<String> palavras = new ArrayList<String>();
-       palavras.add("alura online");
-       palavras.add("editora casa do codigo");
-       palavras.add("caelum");
+        List<String> palavras = new ArrayList<String>();
+        palavras.add("alura online");
+        palavras.add("editora casa do codigo");
+        palavras.add("caelum");
 
-        Comparator<String> comparador = new ComparadorPorTamanho();
-        //Collections.sort(palavras);
-        palavras.sort(comparador);
+//        Comparator<String> comparador = new ComparadorPorTamanho();
+//        //Collections.sort(palavras);
+//        palavras.sort(comparador);
+
+//        palavras.sort((s1, s2) -> {
+//                if (s1.length() < s2.length())
+//                    return -1;
+//                if (s1.length() > s2.length())
+//                    return 1;
+//                return 0;
+//            }
+//        });
+
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
         System.out.println(palavras);
+
+//        Comparator<String> comparador = new ComparadorPorTamanho();
+//        //Collections.sort(palavras);
+//        palavras.sort(comparador);
+//
+//        System.out.println(palavras);
 
 //        for (String p : palavras) {
 //            System.out.println(p);
 //        }
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
-        palavras.forEach(consumidor);
-    }
+//        palavras.forEach(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) {
+//                System.out.println(s);
+//            }
+//        });
+//    }
 
-}
+//        palavras.forEach((String s) -> {
+//                System.out.println(s);
+//            }
+//        );
 
-class ImprimeNaLinha implements Consumer<String> {
+//        palavras.forEach(s -> {
+//                    System.out.println(s);
+//        });
+//        Consumer<String> impressor = s -> System.out.println(s);
+//        palavras.forEach(impressor);
 
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
+        palavras.forEach(s -> System.out.println(s));
     }
 }
 
@@ -41,7 +67,7 @@ class ComparadorPorTamanho implements Comparator<String> {
     public int compare(String s1, String s2) {
         if (s1.length() < s2.length())
             return -1;
-        if(s1.length() > s2.length())
+        if (s1.length() > s2.length())
             return 1;
         return 0;
 
