6597c4ee784cb8c0ca4e50aeb8ab556f1661b5e4
Eduardo Ramos <edugramosf@gmail.com>
2022-06-30 21:53:06 -0300

    Código mais sucinto com Method References

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
     public static void main(String[] args) {
@@ -25,9 +26,22 @@ public class OrdenaStrings {
 //            }
 //        });
 
-        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        //palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+
+        palavras.sort(Comparator.comparing(s -> s.length()));
+        palavras.sort(Comparator.comparing(String::length));
+
+        Function<String, Integer> funcao = s -> s.length();
+        Comparator<String> comparador = Comparator.comparing(s -> s.length());
+        palavras.sort(comparador);
+
+
 
         System.out.println(palavras);
+
+        Consumer<String> impressor = System.out::println;
+                palavras.forEach(System.out::println);
 
 //        Comparator<String> comparador = new ComparadorPorTamanho();
 //        //Collections.sort(palavras);
